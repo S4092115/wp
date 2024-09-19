ee00009a8cdf15660d17ba1acf816a7df43ab0ae
s4092115@student.rmit.edu.au <126431658+Lysitheatsu@users.noreply.github.com>
2024-09-19 11:31:32 +1000

    added links to the new php pages

--- a2/add.php
+++ a2/add.php
@@ -56,10 +56,10 @@
         <img src="images/logo.png" alt="Pets Victoria Logo" class="logo">
         <select id="pageSelect">
             <option value="" disabled selected>Select an Option...</option>
-            <option value="index.html">Home</option>
-            <option value="pets.html">Pets</option>
-            <option value="add.html">Add A Pet</option>
-            <option value="gallery.html">Gallery</option>
+            <option value="index.php">Home</option>
+            <option value="pets.php">Pets</option>
+            <option value="add.php">Add A Pet</option>
+            <option value="gallery.php">Gallery</option>
         </select>
         <input type="search" placeholder="Search">
         <img src="images/searchico.png" alt="Search Icon" class="search-icon">
