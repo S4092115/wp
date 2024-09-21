762ceadecade9cced084283778c7afb0bec7b26c
Lysithea <19flaszatommy@gmail.com>
2024-09-21 20:56:29 +1000

    refixed the sql to original and refixed password for J5 however it keeps coming with errors and ill need to ask teach about this

--- a2/pets.sql
+++ a2/pets.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE petsvictoria;
 USE petsvictoria;
 CREATE TABLE `pets` (
-  `petid` int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `petid` int (11) NOT NULL AUTO_INCREMENT,
   `petname` varchar (255) NOT NULL,
   `description` text NOT NULL,
   `image` varchar (255) NOT NULL,
@@ -9,4 +9,5 @@ CREATE TABLE `pets` (
   `age` double NOT NULL,
   `location` varchar (255) NOT NULL,
   `type` varchar (255) NOT NULL,
+  PRIMARY KEY (`petid`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
