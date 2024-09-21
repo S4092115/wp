40119a776a8be0a31039dfbfeea01e6e0d4193f7
Lysithea <19flaszatommy@gmail.com>
2024-09-21 16:20:36 +1000

    readded the select portion

--- a2/pets.php
+++ a2/pets.php
@@ -5,7 +5,9 @@
         <a href="index.php">
         <img src="images/logo.png" alt="Pets Victoria Logo" class="logo">
         </a>
-        <?php include_once "includes/nav.inc"; ?>
+        <select id="pageSelect">
+            <?php include_once "includes/nav.inc"; ?>
+        </select>
         <input type="search" placeholder="Search">
         <img src="images/searchico.png" alt="Search Icon" class="search-icon">
     </header>
