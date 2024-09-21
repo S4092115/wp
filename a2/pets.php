cff69537b58f7f337b419707370a3c4c1fd58c6f
Lysithea <19flaszatommy@gmail.com>
2024-09-21 16:19:42 +1000

    refixed the nav bar hopefully by replacing the old one

--- a2/pets.php
+++ a2/pets.php
@@ -1,18 +1,11 @@
 <?php include_once "includes/header.inc"; ?>
-<?php include_once "includes/nav.inc"; ?>
 
 <body>
     <header>
         <a href="index.php">
         <img src="images/logo.png" alt="Pets Victoria Logo" class="logo">
         </a>
-        <select id="pageSelect">
-            <option value="" disabled selected>Select an Option...</option>
-            <option value="index.php">Home</option>
-            <option value="pets.php">Pets</option>
-            <option value="add.php">Add A Pet</option>
-            <option value="gallery.php">Gallery</option>
-        </select>
+        <?php include_once "includes/nav.inc"; ?>
         <input type="search" placeholder="Search">
         <img src="images/searchico.png" alt="Search Icon" class="search-icon">
     </header>
